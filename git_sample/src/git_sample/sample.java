17395f49273da4a5069b53c5b48ad1173ab9d612
Jai deep <vallapunenijaideep@gmail.com>
2020-07-21 09:27:12 +0530

    First Commit

---
+++ git_sample/src/git_sample/sample.java
@@ -0,0 +1,11 @@
+package git_sample;
+
+public class sample {
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello Git");
+		for(int i=0;i<10;i++)
+			System.out.println(i);
+	}
+}
